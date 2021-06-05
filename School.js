5959884d5d83e629bf54fcee6c53fd768f2e1c73
DeafWired <37221707+DeathWired@users.noreply.github.com>
2021-06-05 18:56:12 -0400

    Add files via upload

--- School.js
+++ School.js
@@ -1,13 +1,15 @@
 const puppeteer = require('puppeteer');
 let current_datetime = new Date();
 let Formatted_date = (current_datetime.getMonth() + 1) + "/" + current_datetime.getDate();
+//var formatted_date = "10/23"
 console.log(Formatted_date);
 (async () => {
   //Launching/letting you choose to see chrome or not (false/true)
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
+  try {
   //Going to schoology
-  await page.goto('YOUR SCHOOLOGYS LINK HERE');
+  await page.goto('SCHOOLOGY WEBSITE HERE');
   await page.waitForNavigation()
   //Putt your email here
   await page.type('#i0116', 'EMAIL HERE')
@@ -28,9 +30,16 @@ console.log(Formatted_date);
   await page.click('#comment-placeholder')
   await page.type('#comment-placeholder', 'Here')
   await page.click('#edit-submit')
-
-
   await page.setViewport({width:1920, height:1080});
-  await page.screenshot({path: 'example.png', fullpage:true});
+  await page.screenshot({path: 'Submission.png', fullpage:true});
+  console.log('done')
   await browser.close();
+}
+catch{
+  console.log('Error, please look at Submission.png to figure out where something went wrong')
+  await page.setViewport({width:1920, height:1080});
+  await page.screenshot({path: 'Submission.png', fullpage:true});
+  console.log('done')
+  await browser.close();
+}
 })();
